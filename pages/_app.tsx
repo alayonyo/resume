f1f84d9cf0a0bee8f1c27cf8c53f1d9affd81af2
Yonatan Ayalon <yonatan.ayalon@vimeo.com>
2025-12-03 13:19:00 -0500

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,47 @@
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
-import '../styles/globals.css'
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import Script from 'next/script';
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Yonatan Resume</title>
-        <meta name="description" content="Professional resume website built with Next.js" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name='description'
+          content='Professional resume website built with Next.js'
+        />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1'
+        />
+        <link
+          rel='icon'
+          href='/favicon.ico'
+        />
       </Head>
-      <div className="font-sans">
+
+      {/* Google Analytics */}
+      <Script
+        src='https://www.googletagmanager.com/gtag/js?id=G-F5DRLQZC9Z'
+        strategy='afterInteractive'
+      />
+      <Script
+        id='google-analytics'
+        strategy='afterInteractive'
+      >
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-F5DRLQZC9Z');
+        `}
+      </Script>
+
+      <div className='font-sans'>
         <Component {...pageProps} />
       </div>
     </>
-  )
+  );
 }
